e35afd2eeecc648ef47071d6a25292f5ade486b9
gustacaru <gustavoalmeida26@hotmail.com>
2022-06-08 12:47:20 -0300

    update

--- src/api/routes/routes.js
+++ src/api/routes/routes.js
@@ -1,17 +1,17 @@
 const { Router } = require('express');
 const { register, login } = require('../controllers/signInUpController');
-const cntrll = require('../controllers/userInController');
+const { firstIn, newInputs, currentStatus } = require('../controllers/userInController');
 const auth = require('../middlewares/authentication');
 
 const router = Router();
 
-router.get('/', (_req, res) => res.send('vai'));
+router.get('/', (_req, res) => res.json({ message: "yieldCheck landinpage" }));
 
 router.post('/sign-up', register);
 router.post('/sign-in', login);
 
-router.post('/supply/first-setup', auth, cntrll.firstIn);
-router.put('/supply/:id', auth, cntrll.newInputs);
-router.get('/current-status/:id', auth, cntrll.currentStatus);
+router.post('/supply/first-setup', auth, firstIn);
+router.put('/supply/:id', auth, newInputs);
+router.get('/current-status/:id', auth, currentStatus);
 
 module.exports = router;

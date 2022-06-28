7c85468c4fa208560672f2d28309f83e0fcc0f51
gustacaru <gustavoalmeida26@hotmail.com>
2022-06-28 19:03:55 -0300

    add some tests issues

--- src/tests/unit/signInUpService.test.js
+++ src/tests/unit/signInUpService.test.js
@@ -1,6 +1,25 @@
+const { MongoClient } = require('mongodb');
 const service = require('../../api/services/signInUpService');
 
 test('Testa função userRegister recebendo como parâmetro um objeto válido', async () => {
+
+  let conn;
+  let db;
+
+  beforeAll(async () => {
+    conn = await MongoClient.connect(mongoDbUrl,
+      { useNewUrlParser: true, useUnifiedTopology: true }
+    );
+    db = conn.db('kmperl');
+    await db.collection('users').deleteMany({});
+    await db.collection('users-yield').deleteMany({});
+  });
+
+  afterAll(async () => {
+    await conn.close();
+  });
+
+
   const userOkMock = {
     name: "Mano Brown",
     email: "brown@gmail.com",
